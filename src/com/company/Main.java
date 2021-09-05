98320ae232f638f3b54eaccffd60656c71408946
marc572m <marc572m@edu.easj.dk>
2021-09-05 16:12:34 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello world");
+
+    String day = "Monday";
+    int date = 6;
+    String month = "September";
+    int year = 2021;
+
+    System.out.println("American date:");
+    System.out.println(day+ ", "+ month+ " "+ date+ ", "+ year);
+
+    System.out.println("European date:");
+    System.out.println(day+ " "+ date+ " "+ month+ " "+ year);
+
+
+
+
+    }
+}
